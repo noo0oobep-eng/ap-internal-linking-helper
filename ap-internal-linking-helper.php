841cd6bdb4c60f4ea09dcc53c0970e2891f5df76
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-06 19:01:06 +0700

    Update ap-internal-linking-helper.php

--- ap-internal-linking-helper.php
+++ ap-internal-linking-helper.php
@@ -16,3 +16,10 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+define('APILH_VERSION', '0.1.0');
+define('APILH_PATH', plugin_dir_path(__FILE__));
+define('APILH_URL', plugin_dir_url(__FILE__));
+
+require_once APILH_PATH . 'includes/class-apilh-plugin.php';
+
+APILH_Plugin::instance();
