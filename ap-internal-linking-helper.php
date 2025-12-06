4182eb15c11c111b1e3d3bee06c2be4a5b81baf4
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-06 18:55:22 +0700

    Create ap-internal-linking-helper.php

---
+++ ap-internal-linking-helper.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name: AP Internal Linking Helper
+ * Plugin URI: https://github.com/noo0oobep-eng/ap-internal-linking-helper
+ * Description: Lightweight internal linking suggestions inside the block editor.
+ * Version: 0.1.0
+ * Author: AP Systems Lab
+ * Author URI: https://github.com/noo0oobep-eng
+ * License: GPL-2.0-or-later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain: ap-internal-linking-helper
+ * Requires at least: 6.0
+ * Requires PHP: 7.4
+ */
+
+if (!defined('ABSPATH')) {
+    exit;
+}
