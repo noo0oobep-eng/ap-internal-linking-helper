d2838b622e4ede865fcae5a3a70457dc1a126ff4
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-06 19:07:48 +0700

    Update class-apilh-plugin.php

--- includes/class-apilh-plugin.php
+++ includes/class-apilh-plugin.php
@@ -15,9 +15,19 @@ class APILH_Plugin {
 
     private function __construct() {
         add_action('init', [$this, 'init']);
+        add_action('admin_notices', [$this, 'admin_notice']);
     }
 
     public function init() {
         // Future: register editor sidebar, REST endpoints, etc.
     }
+
+    public function admin_notice() {
+        if (!current_user_can('manage_options')) {
+            return;
+        }
+
+        echo '<div class="notice notice-success is-dismissible"><p>AP Internal Linking Helper is active.</p></div>';
+    }
 }
+
