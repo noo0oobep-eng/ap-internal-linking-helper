a665ea638657c977c35540579e079cb20635eb9b
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-07 18:34:25 +0700

    Update ap-internal-linking-helper.php

--- ap-internal-linking-helper.php
+++ ap-internal-linking-helper.php
@@ -7,7 +7,6 @@
  * Author: AP Systems Lab
  * Author URI: https://github.com/noo0oobep-eng
  * License: GPL-2.0-or-later
- * License URI: https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain: ap-internal-linking-helper
  * Requires at least: 6.0
  * Requires PHP: 7.4
@@ -16,6 +15,7 @@
 if (!defined('ABSPATH')) {
     exit;
 }
+
 define('APILH_VERSION', '0.1.0');
 define('APILH_PATH', plugin_dir_path(__FILE__));
 define('APILH_URL', plugin_dir_url(__FILE__));
