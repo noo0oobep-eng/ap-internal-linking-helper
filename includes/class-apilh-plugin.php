86648b4f26846d319a77bcf2bdcb2a8ffcdb045b
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-07 17:35:25 +0700

    Update class-apilh-plugin.php

--- includes/class-apilh-plugin.php
+++ includes/class-apilh-plugin.php
@@ -18,69 +18,70 @@ class APILH_Plugin {
         add_action('enqueue_block_editor_assets', [$this, 'enqueue_editor_assets']);
     }
 
-public function init() {
-    add_action('rest_api_init', [$this, 'register_rest_routes']);
-}
+    public function init() {
+        add_action('rest_api_init', [$this, 'register_rest_routes']);
+    }
 
-public function register_rest_routes() {
-    register_rest_route('apilh/v1', '/suggestions', [
-        'methods'  => 'GET',
-        'callback' => [$this, 'get_suggestions'],
-        'permission_callback' => function () {
-            return current_user_can('edit_posts');
-        },
-        'args' => [
-            'post_id' => [
-                'required' => false,
-                'type'     => 'integer',
-            ],
-        ],
-    ]);
-}
-
-public function get_suggestions($request) {
-    $post_id = (int) $request->get_param('post_id');
-
-    $args = [
-        'post_type'      => ['post', 'page'],
-        'post_status'    => 'publish',
-        'posts_per_page' => 5,
-        'orderby'        => 'date',
-        'order'          => 'DESC',
-        'post__not_in'   => $post_id ? [$post_id] : [],
-        'no_found_rows'  => true,
-    ];
-
-    $posts = get_posts($args);
-
-    $items = array_map(function ($p) {
-        return [
-            'id'    => $p->ID,
-            'title' => get_the_title($p),
-            'link'  => get_permalink($p),
-            'type'  => $p->post_type,
+    public function enqueue_editor_assets() {
+        $handle = 'apilh-editor';
+        $src    = APILH_URL . 'assets/js/editor.js';
+        $deps   = [
+            'wp-plugins',
+            'wp-edit-post',
+            'wp-element',
+            'wp-components',
+            'wp-data',
+            'wp-i18n',
+            'wp-api-fetch',
         ];
-    }, $posts);
+        $ver    = defined('APILH_VERSION') ? APILH_VERSION : '0.1.0';
 
-    return rest_ensure_response([
-        'items' => $items,
-    ]);
+        wp_enqueue_script($handle, $src, $deps, $ver, true);
+    }
+
+    public function register_rest_routes() {
+        register_rest_route('apilh/v1', '/suggestions', [
+            'methods'  => 'GET',
+            'callback' => [$this, 'get_suggestions'],
+            'permission_callback' => function () {
+                return current_user_can('edit_posts');
+            },
+            'args' => [
+                'post_id' => [
+                    'required' => false,
+                    'type'     => 'integer',
+                ],
+            ],
+        ]);
+    }
+
+    public function get_suggestions($request) {
+        $post_id = (int) $request->get_param('post_id');
+
+        $args = [
+            'post_type'      => ['post', 'page'],
+            'post_status'    => 'publish',
+            'posts_per_page' => 5,
+            'orderby'        => 'date',
+            'order'          => 'DESC',
+            'post__not_in'   => $post_id ? [$post_id] : [],
+            'no_found_rows'  => true,
+        ];
+
+        $posts = get_posts($args);
+
+        $items = array_map(function ($p) {
+            return [
+                'id'    => $p->ID,
+                'title' => get_the_title($p),
+                'link'  => get_permalink($p),
+                'type'  => $p->post_type,
+            ];
+        }, $posts);
+
+        return rest_ensure_response([
+            'items' => $items,
+        ]);
+    }
 }
 
-public function enqueue_editor_assets() {
-    $handle = 'apilh-editor';
-    $src    = APILH_URL . 'assets/js/editor.js';
-    $deps   = [
-        'wp-plugins',
-        'wp-edit-post',
-        'wp-element',
-        'wp-components',
-        'wp-data',
-        'wp-i18n',
-        'wp-api-fetch',
-    ];
-    $ver    = defined('APILH_VERSION') ? APILH_VERSION : '0.1.0';
-
-    wp_enqueue_script($handle, $src, $deps, $ver, true);
- }
-}
