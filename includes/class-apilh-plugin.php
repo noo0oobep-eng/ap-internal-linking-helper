9883d580b9a261be1c90a330f1b2c88f31a7c75a
noo0oobep-eng <noo0oobep@gmail.com>
2025-12-07 09:47:33 +0700

    Update class-apilh-plugin.php

--- includes/class-apilh-plugin.php
+++ includes/class-apilh-plugin.php
@@ -22,12 +22,20 @@ class APILH_Plugin {
         // Future: register editor sidebar, REST endpoints, etc.
     }
 
-    public function enqueue_editor_assets() {
-        $handle = 'apilh-editor';
-        $src    = APILH_URL . 'assets/js/editor.js';
-        $deps   = ['wp-plugins', 'wp-edit-post', 'wp-element', 'wp-components', 'wp-data', 'wp-i18n'];
-        $ver    = defined('APILH_VERSION') ? APILH_VERSION : '0.1.0';
+public function enqueue_editor_assets() {
+    $handle = 'apilh-editor';
+    $src    = APILH_URL . 'assets/js/editor.js';
+    $deps   = [
+        'wp-plugins',
+        'wp-edit-post',
+        'wp-element',
+        'wp-components',
+        'wp-data',
+        'wp-i18n',
+        'wp-api-fetch',
+    ];
+    $ver    = defined('APILH_VERSION') ? APILH_VERSION : '0.1.0';
 
-        wp_enqueue_script($handle, $src, $deps, $ver, true);
-    }
+    wp_enqueue_script($handle, $src, $deps, $ver, true);
+ }
 }
